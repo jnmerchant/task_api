87e5b8ce185795f1fb4f8ae19c77e2218035d7cd
jnmerchant <joemercnc@gmail.com>
2017-01-30 22:27:05 -0500

    updated for rubocop

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ end
 
 get '/api/tasks/:id' do |id|
   task = Task.find_by_id(id)
-  halt 404, { message:'Task Not Found'}.to_json unless task
+  halt 404, { message : 'Task Not Found' }.to_json unless task
   task.to_json
 end
 
@@ -30,7 +30,7 @@ put '/api/tasks/:id' do |id|
 end
 
 delete '/api/tasks/:id' do |id|
-  if not Task.exists?(id)
+  if ! Task.exists?(id)
     status 404
   else
     Task.destroy(id)
--- create_task_table.rb
+++ create_task_table.rb
@@ -1,7 +1,6 @@
 require_relative 'migrations/environment'
 
 class CreateTasksTable < ActiveRecord::Migration[5.0]
-
   def up
     create_table :tasks do |t|
       t.string :description
