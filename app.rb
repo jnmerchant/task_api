4443144616fe5045b890ddb8994a279a5233dd77
jnmerchant <joemercnc@gmail.com>
2017-01-31 23:18:29 -0500

    updated for sort, search and filter in endpoints

--- app.rb
+++ app.rb
@@ -1,14 +1,29 @@
 require 'sinatra'
 require_relative 'migrations/environment'
 require_relative 'models/task'
+require 'pry'
+
+after do
+  ActiveRecord::Base.connection.close
+end
 
 get '/api/tasks' do
-  Task.all.to_json
+  tasks = Task.all
+
+  [:id, :description, :priority, :completed].each do |filter|
+    tasks = tasks.send(filter, params[filter]) if params[filter]
+  end
+  tasks.order('priority ASC, created_at DESC').to_json
+end
+
+get '/api/tasks/:sort' do |sort|
+  tasks = Task.all.order(params[sort])
+  tasks.to_json
 end
 
 get '/api/tasks/:id' do |id|
   task = Task.find_by_id(id)
-  halt 404, { message: 'Task Not Found' }.to_json unless task
+  halt(404, {message: "Task not found"}.to_json) unless task
   task.to_json
 end
 
@@ -16,8 +31,7 @@ post '/api/tasks' do
   task = Task.new(description: params[:description], priority: params[:priority])
   if task.valid?
     task.save
-    status 201
-    task.to_json
+    [201, task.to_json]
   else
     status 400
   end
@@ -25,7 +39,8 @@ end
 
 put '/api/tasks/:id' do |id|
   task = Task.find(id)
-  task.update(description: params[:description], priority: params[:priority])
+  halt(404, { message:'Task not found'}.to_json) unless task
+  task.update(description: params[:description], priority: params[:priority], completed: params[:completed])
   task.to_json
 end
 
--- models/task.rb
+++ models/task.rb
@@ -1,5 +1,11 @@
 require 'active_record'
 
 class Task < ActiveRecord::Base
-  validates :description, :priority, presence: true
+  validates :description, presence: true
+  validates :priority, numericality: {only_integer: true}
+
+  scope :id, -> (id) { where(id: id) }
+  scope :description, -> (description) { where("description ILIKE ?", "%#{description}%") }
+  scope :priority, -> (priority) { where(priority: priority) }
+  scope :completed, -> (completed) { where(completed: completed) }
 end
